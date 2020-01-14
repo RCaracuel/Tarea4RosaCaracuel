aba108cf65171e69a33b95e481204c9a61e42826
jose <delossantosramosjose@gmail.com>
2020-01-14 17:39:13 +0100

    Creación de main y objetos y los mostramos

---
+++ src/main/java/tarea4/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea4;
+
+/**
+ *
+ * @author Jose
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Perro Pipo = new Perro(00001, "Caniche", "Blanco");
+        Perro Tobi = new Perro(00002, "Chiguagua", "Marrón");
+
+        System.err.println(Pipo);
+        System.err.println(Tobi);
+
+    }
+}
